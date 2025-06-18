6c02b0ca7048eb69a7b924c7c9fe9f0bd805db26
romsper <r.speranskii@gmail.com>
2025-06-17 23:13:43 -0400

    Updated all libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.9.1"
+    id("com.android.application") version "8.9.3"
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
     id("org.jetbrains.kotlin.android") version "2.1.20"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
@@ -45,11 +45,11 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
 
     // Network
-    val retrofitVersion = "2.11.0"
+    val retrofitVersion = "3.0.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-kotlinx-serialization:$retrofitVersion")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.16")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 
     // DB
     val roomVersion = "2.7.1"
@@ -57,24 +57,24 @@ dependencies {
     implementation("androidx.room:room-ktx:$roomVersion")
 
     // DI
-    implementation("io.insert-koin:koin-androidx-compose:4.1.0-Beta8")
+    implementation("io.insert-koin:koin-androidx-compose:4.1.0")
 
     // Coil 3
-    val coilVersion = "3.1.0"
+    val coilVersion = "3.2.0"
     implementation("io.coil-kt.coil3:coil-compose:$coilVersion")
     implementation("io.coil-kt.coil3:coil-network-okhttp:$coilVersion")
 
     // Lottie
-    val lottieVersion = "6.6.4"
+    val lottieVersion = "6.6.6"
     implementation("com.airbnb.android:lottie-compose:$lottieVersion")
 
     // UI
-    implementation(platform("androidx.compose:compose-bom:2025.05.00"))
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.06.00"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+    implementation("androidx.activity:activity-compose:1.12.0-alpha02")
     implementation("androidx.navigation:navigation-compose:2.9.0")
-    implementation("androidx.compose.material3:material3:1.4.0-alpha14")
+    implementation("androidx.compose.material3:material3:1.4.0-alpha15")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.2")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
 }
