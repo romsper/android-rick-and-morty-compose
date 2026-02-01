3867b51cae3c1738182028a2531232f544d53582
romsper <r.speranskii@gmail.com>
2026-02-01 18:44:38 -0500

    Update HomeScreen.kt

--- app/src/androidTest/java/com/example/rickandmorty/screens/HomeScreen.kt
+++ app/src/androidTest/java/com/example/rickandmorty/screens/HomeScreen.kt
@@ -1,8 +1,16 @@
 package com.example.rickandmorty.screens
 
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsNode
+import androidx.compose.ui.semantics.SemanticsPropertyKey
+import androidx.compose.ui.semantics.SemanticsPropertyReceiver
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import com.kaspersky.components.composesupport.core.KNode
 import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.github.kakaocup.compose.node.element.lazylist.KLazyListItemNode
+import io.github.kakaocup.compose.node.element.lazylist.KLazyListNode
 
 class HomeScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<HomeScreen>(
@@ -11,4 +19,40 @@ class HomeScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ) {
 
     val title: KNode = child { hasTestTag("titleTopAppBar") }
+
+    val list = KLazyListNode(
+        semanticsProvider = semanticsProvider,
+        viewBuilderAction = { hasTestTag("LazyList") },
+        itemTypeBuilder = {
+            itemType(::LazyListItemNode)
+            itemType(::LazyListHeaderNode)
+        },
+        positionMatcher = { position ->
+            SemanticsMatcher.expectValue(
+                LazyListItemPosition,
+                position
+            )
+        }
+    )
+}
+
+val LazyListItemPosition = SemanticsPropertyKey<Int>("LazyListItemPosition")
+var SemanticsPropertyReceiver.lazyListItemPosition by LazyListItemPosition
+
+fun Modifier.lazyListItemPosition(position: Int): Modifier {
+    return semantics { lazyListItemPosition = position }
+}
+
+class LazyListItemNode(
+    semanticsNode: SemanticsNode,
+    semanticsProvider: SemanticsNodeInteractionsProvider,
+) : KLazyListItemNode<LazyListItemNode>(semanticsNode, semanticsProvider)
+
+class LazyListHeaderNode(
+    semanticsNode: SemanticsNode,
+    semanticsProvider: SemanticsNodeInteractionsProvider,
+) : KLazyListItemNode<LazyListHeaderNode>(semanticsNode, semanticsProvider) {
+    val title: KNode = child {
+        hasTestTag("LazyListHeaderTitle")
+    }
 }
